c035150078e6a44cdc14e0bb0f2007bdd6950f66
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2019-03-25 21:19:08 +0530

    Initial commit

---
+++ src/main/java/com/frontline/testdsmms/TestdsmmsApplication.java
@@ -0,0 +1,13 @@
+package com.frontline.testdsmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdsmmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestdsmmsApplication.class, args);
+    }
+
+}
