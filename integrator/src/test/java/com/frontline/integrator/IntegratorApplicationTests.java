544884e07960a8d78f0122bcf237a8042a56b66b
supimi <supimi.15@cse.mrt.ac.lk>
2019-04-01 14:46:33 +0530

    multiple repos added

--- integrator/src/test/java/com/frontline/integrator/IntegratorApplicationTests.java
+++ integrator/src/test/java/com/frontline/integrator/IntegratorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.frontline.testdsmms;
+package com.frontline.integrator;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestdsmmsApplicationTests {
+public class IntegratorApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
---
+++ multiplicator/src/main/java/com/frontline/multiplicator/MultiplicatorApplication.java
@@ -0,0 +1,13 @@
+package com.frontline.multiplicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplicatorApplication.class, args);
+	}
+
+}
